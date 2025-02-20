0d9bda9d121bd91bb8605ad1309ec1a185a2c0f2
Guilherme Santos Urbano <guisanto@c1r7s13.42lisboa.com>
2025-02-20 15:52:09 +0000

    :tada:

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: guisanto <guisanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 15:08:49 by guisanto          #+#    #+#             */
-/*   Updated: 2025/02/03 16:43:00 by guisanto         ###   ########.fr       */
+/*   Updated: 2025/02/20 14:47:50 by guisanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,60 +14,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft/libft.h"
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(void)
-{
-    int pipefd[2];
-    pid_t pid1, pid2;
 
-    //criando o pipe
-    if (pipe(pipefd) == -1)
-    {
-        perror("pipe");
-        exit(1);
-    }
-    //primeiro Fork (processo 1)
-    pid1 = fork();
-    if (pid1 == -1)
-    {
-        perror("fork");
-        exit(1);
-    }
-    if (pid1 == 0) //processo 1
-    {
-        //redirecionar saida para o pipe
-        close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[1]);
-        
-        char *cmd1[] = {"cat", "file.txt", NULL};
-        execvp(cmd1[0], cmd1);
-        perror("execvp");
-        exit(1);
-    }
-    pid2 = fork();
-    if (pid1 == -1)
-    {
-        perror("fork2");
-        exit(1);
-    }
-    if (pid2 == 0)
-    {
-        close(pipefd[1]);
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[0]);
-        
-        //executar o grep
-        char *cmd2[] = {"grep", "something", NULL};
-        execvp(cmd2[0], cmd2);
-        perror("execvp2");
-        exit(1);
-    }
-    close(pipefd[0]);
-    close(pipefd[1]);
-    
-    //esperando os filhos terminarem
-    wait(NULL);
-    wait(NULL);
-    return (0);
+int main() {
+	int pipe_fd[2];  // Array para armazenar os descritores do pipe
+	pid_t pid1, pid2;
+
+	// Criando o pipe
+	if (pipe(pipe_fd) == -1) {
+		perror("pipe");
+		exit(1);
+	}
+
+	// Primeiro processo
+	pid1 = fork();
+	if (pid1 == -1) {
+		perror("fork");
+		exit(1);
+	}
+
+	if (pid1 == 0) {  // Filho 1: Produzindo a saída
+		close(pipe_fd[0]);  // Fechar a leitura do pipe
+		dup2(pipe_fd[1], STDOUT_FILENO);  // Redireciona a saída padrão para o pipe
+		close(pipe_fd[1]);  // Não precisamos mais do descritor do pipe
+
+		execlp("ls", "ls", NULL);  // Executa o comando "ls"
+		perror("execlp");  // Se execlp falhar
+		exit(1);
+	}
+
+	// Segundo processo
+	pid2 = fork();
+	if (pid2 == -1) {
+		perror("fork");
+		exit(1);
+	}
+
+	if (pid2 == 0) {  // Filho 2: Consumindo a saída
+		close(pipe_fd[1]);  // Fechar a escrita do pipe
+		dup2(pipe_fd[0], STDIN_FILENO);  // Redireciona a entrada padrão para o pipe
+		close(pipe_fd[0]);  // Não precisamos mais do descritor do pipe
+
+		execlp("grep", "grep", "txt", NULL);  // Executa o comando "grep txt" com a saída de "ls"
+		perror("execlp");  // Se execlp falhar
+		exit(1);
+	}
+
+	// Processo pai: Espera os filhos
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	wait(NULL);  // Espera o primeiro filho
+	wait(NULL);  // Espera o segundo filho
+
+	return 0;
 }
